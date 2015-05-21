414775e0ba6647936aee6e2f7f10e63a09d89716
Tom Lisi <lisit@wit.edu>
2015-05-21 10:03:41 -0400

    # This is a combination of 4 commits. # The first commit's message is: # This is a combination of 4 commits. # The first commit's message is: # This is a combination of 5 commits. # The first commit's message is: implement existUser endpoint
    # The 2nd commit message will be skipped:
    
    #	add database
    
    # The 3rd commit message will be skipped:
    
    #	add NewUser endpoint
    
    # The 4th commit message will be skipped:
    
    #	better errors and docs for new user endpoint
    
    # The 5th commit message will be skipped:
    
    #	added comments
    
    # The 2nd commit message will be skipped:
    
    #	hufdsa
    
    # The 3rd commit message will be skipped:
    
    #	yooooooooooooooooooooooo
    
    # The 4th commit message will be skipped:
    
    #	all about that yayo
    
    # The 2nd commit message will be skipped:
    
    #	ghhhjhj
    
    # The 3rd commit message will be skipped:
    
    #	asddc
    
    # The 4th commit message will be skipped:
    
    #	for realz

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 
@@ -7,7 +8,112 @@ var port = Number(process.env.PORT || 53535);
 app.listen(port);
 console.log("Listening on port: "+port);
 
+var connectQuery = process.env.DBCONNECT;
+
+MongoClient.connect(connectQuery, function(err, db) {
+  if(err) {
+    console.log("error connecting to the database");
+    console.log(err);
+    return;
+  }
+  var collection = db.collection('pio-api-collection');
+});
+
 
 app.get('/api', function(req, res) {
-  res.send(200);
+  res.send({name:req.query.name});
 });
+
+
+/**
+ * A new user consists of an email, a password, and a type all encoded to protect the user's data
+ *
+ * ex. pio-api.heroku.com/api/newUser?email=kusdkdjfgskjdgfjxg&pass=skaskdjhkdjhksjdjhf&type=yuegjhbn
+ */
+app.get('/api/users/new', function(req, res) {
+  
+  if(!(req.query.email && req.query.pass && req.query.type)) {
+    // invalid user post
+    var badData = undefined;
+    if(!req.query.email) {
+      badData = "email";
+    } else if(!req.query.pass) {
+      badData = "pass";
+    } else if(!req.query.type) {
+      badData = "type";
+    }
+    // 400: Bad Data
+    res.status(400).send({
+      error:"bad data: "+badData
+    });
+    return;
+  }
+  
+  // create a user object to be stored in the database
+  var user = {
+    email: req.query.email,
+    pass: req.query.pass,
+    type: req.query.type
+  };
+  
+  // connect to the database
+  MongoClient.connect(connectQuery, function(err, db) {
+    if (err) {
+      console.log("error connecting to the database for: "+req.ip);
+      console.log(err);
+      // 500: internal server error
+      res.status(500).send({
+        error:"Could not connect to database, see server logs or contact admin"
+      });
+      return;
+    }
+    
+    // get the collection
+    var collection = db.collection('pio-api-collection');
+    
+    // update the collection by adding the user object to the profiles array
+    collection.update({'name':'profiles'}, {$push:{profiles:user}}, function(err, result) {
+      if(err) {
+        console.log("couldnt save user data for "+JSON.stringify(user));
+        console.log(err);
+        // 500: internal server error
+        res.status(500).send({
+          error:"Could not connect to database, see server logs or contact admin"
+        });
+        return    
+      }
+      // it worked
+      res.status(200).send({exist:true});
+    });
+  });
+});
+
+app.get('/api/users/exist', function(req, res) 
+{
+  var email = req.query.email;
+  
+  MongoClient.connect(connectQuery, function(err, db) {
+  if(err) {
+    console.log("error connecting to the database");
+    console.log(err);
+    return;
+  }
+  
+  var emailCollection = collection.find(
+  {
+    'name':'profiles',
+    'profiles':{
+      $in: [
+      {
+        'email':email
+      }
+    ]
+    }
+  }
+);
+
+if(email != NULL){
+  res.status(200).send({exist:true});
+}
+ 
+}
