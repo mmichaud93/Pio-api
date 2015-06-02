b7c00467e30513e2e0204238c6a05991f261e46e
mmichaud <mmichaud@millennialmedia.com>
2015-06-02 10:46:02 -0400

    add NewUser endpoint

--- app.js
+++ app.js
@@ -24,14 +24,36 @@ app.get('/api', function(req, res) {
   res.send({name:req.query.name});
 });
 
+// pio-api.heroku.com/api/newUser?email=kusdkdjfgskjdgfjxg&pass=skaskdjhkdjhksjdjhf
 app.get('/api/newUser', function(req, res) {
+  
+  var user = {
+    email: req.query.email,
+    pass: req.query.pass
+  };
+  
   MongoClient.connect(connectQuery, function(err, db) {
-    if(err) {
+    if (err) {
       console.log("error connecting to the database for: "+req.ip);
       console.log(err);
       return;
     }
     
+    var collection = db.collection('pio-api-collection');
+    
+    
+    
+    collection.update({'name':'profiles'}, {$push:{profiles:user}}, function(err, result) {
+      if(err) {
+        console.log("couldnt save user data for "+JSON.stringify(user));
+        console.log(err);
+        return    
+      }
+    });
+  });
+  
+  res.send({msg: "i tried"});
+});
   //   var collection = db.collection('profiles');
   //   // get the document for the user
   //   collection.findOne({'user':user}, function(err, item) {
@@ -58,8 +80,3 @@ app.get('/api/newUser', function(req, res) {
   //       }
   //     });
   //   });
-  });
-  
-  
-  
-});
